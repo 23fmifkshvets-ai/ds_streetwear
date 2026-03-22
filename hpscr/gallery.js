8289a4832330d8468f77c6925211241237f51c58
23fmifkshvets-ai <23fmif.k.shvets@std.udu.edu.ua>
2026-03-22 23:07:18 +0200

    Update gallery.js

--- hpscr/gallery.js
+++ hpscr/gallery.js
@@ -43,12 +43,26 @@ function prevImage() {
 
 
 
-
+// COLOR
+document.querySelectorAll('.color-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        document.querySelectorAll('.color-btn').forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
+// SIZE
 document.querySelectorAll('.size-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        document.querySelectorAll('.size-btn').forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
+
+/*document.querySelectorAll('.size-btn').forEach(button => {
     button.addEventListener('click', () => {
         // Прибираємо активний клас у всіх
         document.querySelectorAll('.size-btn').forEach(btn => btn.classList.remove('active'));
         // Додаємо поточному
         button.classList.add('active');
     });
-});
+});*/
