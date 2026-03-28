ac4551b27d147d5addbcf3356d708ac681ce20fc
23fmifkshvets-ai <23fmif.k.shvets@std.udu.edu.ua>
2026-03-28 20:42:33 +0200

    Add files via upload

---
+++ hpscr/clwn.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".logo-link").forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+        });
+    });
+});
+
+
+
+const images = [
+    "../../img/clwn/white_cl_bk.jpg",
+    "../../img/ct-wb/black_cl_f.jpg",
+    "../../img/ct-wb/black_cl_b.jpg",
+    "../../img/ct-wb/black_cl_f2.jpg"
+];
+
+let currentImage = 0;
+
+function nextImage() {
+    currentImage++;
+
+    if (currentImage >= images.length) {
+        currentImage = 0;
+    }
+
+    document.getElementById("product-image").src = images[currentImage];
+}
+
+function prevImage() {
+    currentImage--;
+
+    if (currentImage < 0) {
+        currentImage = images.length - 1;
+    }
+
+    document.getElementById("product-image").src = images[currentImage];
+}
+
+
+
+
+// COLOR
+document.querySelectorAll('.color-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        document.querySelectorAll('.color-btn').forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
+// SIZE
+document.querySelectorAll('.size-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        document.querySelectorAll('.size-btn').forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
+
